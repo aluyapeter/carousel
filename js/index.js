bc0d7b70f9d61b681cfd520e773b2450b632557f
perofrontend <aluyapeter7@gmail.com>
2023-04-04 00:05:53 +0100

    testimonies carousel deploy beta

--- js/index.js
+++ js/index.js
@@ -15,17 +15,17 @@ faqsBtn.addEventListener("click", function () {
     faqsAns.style.display = "none"
   }
 })
-// faqsBtn.addEventListener("click", function () {
-//   let faqsAns = document.getElementById("faqs-answer");
+// carousel indicator
 
-//   if (faqsAns.style.display = "block") {
-//     faqsAns.style.display = "none";
-//   }
-// })
+const carouselInner = document.querySelector('.carousel-inner');
+const indicators = document.querySelectorAll('.indicator');
+let activeIndex = 0;
+indicators.forEach((indicator, index) => {
+  indicator.addEventListener('click', () => {
+    carouselInner.style.transform = `translateX(-${index * 100}%)`;
+    indicators[activeIndex].classList.remove('active');
+    indicator.classList.add('active');
+    activeIndex = index;
+  });
+});
 
-// const dropdownToggle = document.querySelector('.dropdown-toggle');
-// const dropdownMenu = document.querySelector('.dropdown-menu');
-
-// dropdownToggle.addEventListener('click', () => {
-//   dropdownMenu.classList.toggle('show');
-// });
